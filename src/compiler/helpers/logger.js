a939d40ee74c2ed7d6736616f048706a3df6d996
johannes <joe.wipper@gmx.de>
2022-10-15 02:18:54 +0200

    chalk doesn't throw anymore on bun

--- src/compiler/helpers/logger.js
+++ src/compiler/helpers/logger.js
@@ -1,13 +1,12 @@
 import console from '../../utils/console.js'
 import {performance} from 'perf_hooks'
+import chalk from 'chalk'
 
 export function showCompilationStatus(label) {
     return {
         name: 'simple-status',
         async setup(build) {
             let start, isFirstCompilation = true, runningMessage
-            const isBun = typeof Bun !== 'undefined' // todo: remove when chalk works on bun
-            const chalk = !isBun && (await import('chalk')).default
             const durationDecimalPlaces = 2
             const compilerName = 'Compiler'
 
@@ -15,7 +14,7 @@ export function showCompilationStatus(label) {
                 start = performance.now()
                 // show compiling in console
                 runningMessage = console.spinner([
-                        isBun ? `${compilerName}:` : chalk.blue(`${compilerName}:`),
+                        chalk.blue(`${compilerName}:`),
                         'Compiling',
                         label,
                         (!isFirstCompilation && 'changes'),
@@ -24,20 +23,21 @@ export function showCompilationStatus(label) {
 
                 runningMessage.color = 'cyan'
             })
-            build.onEnd(result => {
+            build.onEnd(() => {
                 const duration = Math.round(
                     (performance.now() - start) * 10 ** durationDecimalPlaces
                 ) / (10 ** durationDecimalPlaces)
                 // show compiling complete in console
                 runningMessage.stopAndPersist({
-                    text: [isBun ? `${compilerName}:` : chalk.blue(`${compilerName}:`),
+                    text: [
+                        chalk.blue(`${compilerName}:`),
                         'Compiled',
                         label,
                         (!isFirstCompilation && 'changes'),
                         'in',
-                        isBun ? `${duration} ms` : chalk.bold(`${duration} ms`)
+                        chalk.bold(`${duration} ms`)
                     ].filter(v => v).join(' '),
-                    symbol: isBun ? '✓' : chalk.green('✓')
+                    symbol: chalk.green('✓')
                 })
                 runningMessage = null
                 isFirstCompilation = false
