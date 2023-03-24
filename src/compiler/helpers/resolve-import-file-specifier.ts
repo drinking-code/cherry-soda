fc5dd2c35725db70db3769cabc5208ec82f1dd88
johannes <joe.wipper@gmx.de>
2023-03-24 01:56:19 +0100

    fix bugs with use as module (cli, file resolution)

--- src/compiler/helpers/resolve-import-file-specifier.ts
+++ src/compiler/helpers/resolve-import-file-specifier.ts
@@ -1,13 +1,17 @@
 import resolveFile from './resolve-file'
 import projectRoot, {resolve as resolveProjectRoot} from '../../utils/project-root'
+import moduleRoot, {resolve as resolveModuleRoot} from '../../utils/module-root'
 import {printWarning, messages as warningMessages} from '../../messages/warnings'
 
-const packageJson = await (Bun.file(resolveProjectRoot('package.json')) as File & { json: () => object }).json()
+const projectPackageJson = await (Bun.file(resolveProjectRoot('package.json')) as File & { json: () => object }).json()
+const modulePackageJson = await (Bun.file(resolveModuleRoot('package.json')) as File & { json: () => object }).json()
 
 export default function resolveImportFileSpecifier(base: string, fileSpecifier: string) {
     if (fileSpecifier.startsWith('#')) {
-        if ('imports' in packageJson)
-            fileSpecifier = resolveProjectRoot(packageJson.imports[fileSpecifier])
+        if ('imports' in projectPackageJson)
+            fileSpecifier = resolveProjectRoot(projectPackageJson.imports[fileSpecifier])
+        else if ('imports' in modulePackageJson)
+            fileSpecifier = resolveModuleRoot(modulePackageJson.imports[fileSpecifier])
         else
             printWarning(warningMessages.resolve.noImports, [fileSpecifier])
     } else {
--- src/utils/module-root.js
+++ src/utils/module-root.js
@@ -5,5 +5,5 @@ process.env.APP_MODULE_PATH = findDirWith('package.json')
 export default process.env.APP_MODULE_PATH
 
 export function resolve(...filePath) {
-    return path.join(process.env.PROJECT_ROOT_PATH, ...filePath)
+    return path.join(process.env.APP_MODULE_PATH, ...filePath)
 }
