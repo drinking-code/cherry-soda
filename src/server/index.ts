592e93f95f293050d8aa823b9b8c677e9cff5066
johannes <joe.wipper@gmx.de>
2023-04-01 02:18:34 +0200

    open url only on first load (for --hot)

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,11 +4,16 @@ import open from 'open'
 
 import cherrySoda from './bun'
 
+global.count = global.count ?? -1
+global.count++
+
 Bun.serve({
     fetch: cherrySoda(process.env.CHERRY_COLA_ENTRY),
     port: Number(process.env.PORT),
 })
 
 const url = `http://localhost:${process.env.PORT}`
-console.log(chalk.magenta('dev server: ') + `listening at ${url}`)
-// setTimeout(() => open(url), 200)
+if (global.count === 0) {
+    console.log(chalk.magenta('dev server: ') + `listening at ${url}`)
+    setTimeout(() => open(url), 200)
+}
